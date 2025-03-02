2e3605582d6222413c09dd6069701121bebd77de
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-03-02 16:36:09 +0545

    Day 30: Mini-app completion

---
+++ flutter_progress_revision/day30.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Scaffold(
+  appBar: AppBar(title: Text('Day 30: Mini App')),
+  body: Center(child: Text('Congrats! 30 days of Flutter.')),
+)));
